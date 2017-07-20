9c92094f39aea8eb361c2602b4cee41a7a24ccb0
Benjamin <bhoffman8@gmail.com>
2017-07-19 19:34:32 -0700

    adds eslint/standard/prettier

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './tabs.css'
 
-
 const Tabs = ({ tabs, onTabClick }) => {
   return (
     <div className={styles.wrapper}>
-      {tabs.map(tab => <div className={styles.tab} onClick={()=> onTabClick(tab.value)}>{tab.name}</div>)}
+      {tabs.map(tab =>
+        <div className={styles.tab} onClick={() => onTabClick(tab.value)}>
+          {tab.name}
+        </div>
+      )}
     </div>
   )
 }
