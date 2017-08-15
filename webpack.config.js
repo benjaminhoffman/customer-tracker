7cc55e393e493afcb3411e220350634711512a3b
Benjamin <bhoffman8@gmail.com>
2017-08-14 18:45:36 -0700

    splits into dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,129 @@
-const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+const Merge = require('webpack-merge')
+const env = process.env.NODE_ENV || 'development'
+const ManifestPlugin = require('webpack-manifest-plugin')
 
-console.log(process.env)
-module.exports = {
+const isProductionLike = env === 'production' || env === 'staging'
+
+console.log('isProductionLike', isProductionLike)
+
+const common = {
   entry: {
-    index: './src/client/index.js'
+    app: './src/client/index.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(
+        isProductionLike ? 'production' : 'development'
+      )
+    }),
+    // generates our html file
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html',
+      filename: 'index.html'
+    }),
+    // creates a file for all shared (common) modules across entry points
+    // loads once and uses cache for pagespeed optimizations
+    // webpack.js.org/plugins/commons-chunk-plugin/
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
+    })
+  ],
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/'
+    publicPath: '/',
+    sourceMapFilename: 'bundle.map'
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            plugins: ['recharts'],
-            presets: ['env', 'es2015', 'react']
+            presets: ['env', 'stage-0']
           }
         }
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader', // needed for css modules
+          {
+            loader: 'style-loader'
+          },
           {
             loader: 'css-loader',
             options: {
-              modules: true, // enables for css modules
-              importLoaders: 1,
-              localIdentName: '[path][name]_[local]_[hash:5]' // more descriptive class names
+              modules: true,
+              camelCase: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
             }
-          },
-          'postcss-loader'
+          }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
       }
     ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR
-    new HtmlWebpackPlugin({
-      template: './src/client/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ],
-  devServer: {
-    hot: true, // enable HMR
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    proxy: {
-      '/': `${process.env.BASE_URL || 'http://localhost'}:${process.env.PORT ||
-        3001}`
-    }
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 }
+
+const development = Merge(common, {
+  devtool: 'cheap-module-eval-source-map', // webpack.js.org/configuration/devtool/#development
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    sourceMapFilename: '[name].map'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']), // replace dist dir on each new build
+    new webpack.HotModuleReplacementPlugin() // Enable HMR
+  ],
+  devServer: {
+    hot: true, // use HMR
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+})
+
+const production = Merge(common, {
+  devtool: 'nosources-source-map', // webpack.js.org/configuration/devtool/#production
+  output: {
+    // use [chunkhash] for asset caching
+    filename: '[name].[chunkhash:8].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    sourceMapFilename: '[name].[chunkhash:8].map'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      comments: false
+    }),
+    new ManifestPlugin()
+  ]
+})
+
+module.exports = isProductionLike ? production : development
