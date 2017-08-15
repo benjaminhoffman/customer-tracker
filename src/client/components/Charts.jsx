d8c466d65dc883e7ae9b9821bcac67c1b79f4568
Benjamin <bhoffman8@gmail.com>
2017-08-15 15:03:04 -0700

    adds AreaChart, moves to chart dir

--- src/client/components/Charts.jsx
+++ src/client/components/Charts.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-import Chart from './Chart'
+import React, { Component } from 'react'
+import Chart from './charts/Chart'
+import AreaChart from './charts/AreaChart'
+import axios from 'axios'
 
-const Charts = () => {
-  return <Chart />
+class Charts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <AreaChart />
+        <Chart />
+      </div>
+    )
+  }
+
+  componentWillMount() {
+    console.log('chart comp mounted!')
+
+    axios
+      .get('http://localhost:3001/api/chart-data')
+      .then(({ data }) => {
+        console.log(data)
+        this.setState({
+          data
+        })
+      })
+      .catch(e => {
+        // TODO do something
+        console.log(e)
+      })
+  }
 }
 
 export default Charts
