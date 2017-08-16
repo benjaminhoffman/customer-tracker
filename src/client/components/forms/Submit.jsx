8975203624aa9f177dfd57c37cfb44d55c2cb664
Benjamin <bhoffman8@gmail.com>
2017-08-16 13:30:40 -0700

    adds Submit component + moves Forms

---
+++ src/client/components/forms/Submit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Form.css'
+
+const Submit = () => {
+  return (
+    <div className={styles.submit}>
+      <input type="submit" value="Submit" />
+    </div>
+  )
+}
+
+export default Submit
